0d18a2eb47026d2d7986c23dc981a1feacfea781
casoon <joern.seidel@casoon.de>
2021-07-28 15:48:18 +0200

    customize html minify

--- images/filters/htmlmin.js
+++ images/filters/htmlmin.js
@@ -3,9 +3,15 @@ const htmlmin = require('html-minifier');
 module.exports = function(content, outputPath) {
     if (outputPath.indexOf('.html') > -1) {
         let minified = htmlmin.minify(content, {
-            useShortDoctype: true,
-            removeComments: true,
+            removeAttributeQuotes: true,
+            collapseBooleanAttributes: true,
             collapseWhitespace: true,
+            removeComments: true,
+            sortClassName: true,
+            sortAttributes: true,
+            html5: true,
+            decodeEntities: true,
+            removeOptionalTags: true,
         });
         return minified;
     }
